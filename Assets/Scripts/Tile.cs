4c76ecca381540f5c912de68a7867c3a13700c66
Alfian Hamdani <alfian.hamdani04@gmail.com>
2020-09-25 20:15:39 +0800

    Calculate tile angle

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    private Vector3 firstPosition,
+        finalPosition;
+    private float swipeAngle;
+    
+    private void OnMouseDown()
+    {
+        // Get first point
+        firstPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void OnMouseUp()
+    {
+        // Get final point
+        finalPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        CalculateAngle();
+    }
+    
+    /// <summary>
+    /// Calculate angle
+    /// </summary>
+    private void CalculateAngle()
+    {
+        // Calculate angle between first position and final position
+        swipeAngle = Mathf.Atan2(finalPosition.y - firstPosition.y, finalPosition.x - firstPosition.x) * 180 / Mathf.PI;
+        
+        MoveTile();
+    }
+    
+    /// <summary>
+    /// Detect swipe angle
+    /// </summary>
+    private void MoveTile()
+    {
+        if (swipeAngle > -45 && swipeAngle <= 45)
+        {
+            // Right swipe
+            Debug.Log("Right swipe");
+        }
+        else if (swipeAngle > 45 && swipeAngle <= 135)
+        {
+            // Up swipe
+            Debug.Log("Up swipe");
+        } 
+        else if (swipeAngle > 135 || swipeAngle <= -135)
+        {
+            // Left swipe
+            Debug.Log("Left swipe");
+        }
+        else if (swipeAngle < -45 && swipeAngle >= -135)
+        {
+            // Down swipe
+            Debug.Log("Down swipe");
+        }
+    }
+}
